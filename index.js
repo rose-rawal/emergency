de5192eb633bcf43fb8a2d1f09662c1c4f32b0b0
rose <rooserwl@gmail.com>
2023-06-10 22:19:42 +0545

    backend socket io prac

--- index.js
+++ index.js
@@ -5,9 +5,14 @@ import dotenv from 'dotenv';
 import router from "./routes/index.js";
 import cors from 'cors';
 import serverrouter from "./routes/server.js";
+import http from 'http';
+import { Server} from "socket.io";
+
 // import router from './routes'
 dotenv.config();
 const app=express();
+const server=http.createServer(app)
+const io=new Server(server);
 mongoose.connect("mongodb://127.0.0.1:27017/userdata")
 .then(()=>{
     console.log("connected to db")
@@ -21,10 +26,17 @@ app.use(cors({origin: true, credentials: true}))
 app.use('/',router)
 app.use('/server/',serverrouter)
 // app.use('/hello',router)
+
 app.post('/hello',(req,res)=>{
    res.send("hello",router)
 })
 export default ServerlessHttp(app)
 app.listen(5000,()=>{
     console.log("server on 5000")
+})
+io.on('connection',()=>{
+    console.log("user is connected");
+})
+server.listen(4000,()=>{
+    console.log("socket server at 4000")
 })
