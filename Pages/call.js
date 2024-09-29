5e31048c8c486041c0e95cc4a290dc4b92196c80
ShivahariRukesh <478gnujnoej@gmail.com>
2024-09-29 08:07:04 +0545

    Adjusted bottom navbar and some bugs

--- Pages/call.js
+++ Pages/call.js
@@ -1,114 +1,136 @@
-import React from 'react'
-import {StyleSheet, Pressable, Text, View,Image, Linking, Platform, ScrollView } from 'react-native'
-import Application from "../layout/application"
+import React from "react";
+import {
+  StyleSheet,
+  Pressable,
+  Text,
+  View,
+  Image,
+  Linking,
+  Platform,
+  ScrollView,
+} from "react-native";
+import Application from "../layout/application";
 const styles = StyleSheet.create({
-  head:{
-    fontSize:20,
-    textAlign:"center",
-    paddingBottom:30,
-    paddingTop:20,
-    fontWeight:"bold",
-    color:'white'
+  head: {
+    fontSize: 20,
+    textAlign: "center",
+    paddingBottom: 30,
+    paddingTop: 20,
+    fontWeight: "bold",
+    color: "white",
   },
-    container: {
-        flex: 1,
-        
-        flexDirection: 'row',
-        flexWrap: 'wrap',
-        justifyContent: 'space-evenly',
-        alignItems: 'center',
-        backgroundColor: '#001133'
-      
-    },
-    pressable: {
-        width: '45%', 
+  container: {
+    flex: 1,
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "space-evenly",
+    alignItems: "center",
+    backgroundColor: "#001133",
+  },
+  pressable: {
+    width: "45%",
     aspectRatio: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
     // padding: 10,
-    paddingBottom:10,
+    paddingBottom: 10,
     marginBottom: 30,
-    backgroundColor: '#FFFFFF',
+    backgroundColor: "#FFFFFF",
     borderRadius: 10,
     elevation: 3,
-      overflow:'hidden'
-    },
-    text: {
-      marginLeft: 10,
-      fontSize: 18,
-      fontWeight: 'bold',
-      color: '#000000',
-    },
-    image: {
-      width: '100%',
-      height: '95%',
-      // borderRadius:10
-    },
-    pics:{
-      // overflow:'hidden',
+    overflow: "hidden",
+  },
+  text: {
+    marginLeft: 10,
+    fontSize: 18,
+    fontWeight: "bold",
+    color: "#000000",
+  },
+  image: {
+    width: "100%",
+    height: "95%",
+    // borderRadius:10
+  },
+  pics: {
+    // overflow:'hidden',
     // height:10
-    }
-  });
-
-
+  },
+});
 
 const Call = () => {
-
   const makePhoneCall = (number) => {
     const phoneNumber = number; // Replace with the actual phone number
-    if (Platform.OS === 'android') {
-    Linking.openURL(phoneNumber);
+    if (Platform.OS === "android") {
+      Linking.openURL(phoneNumber);
     }
-         
-          
   };
   return (
-<>
-<Application>
-<Text style={styles.head}>Available Emergency Call Service</Text>
-<ScrollView>
+    <>
+      <Application>
+        <Text style={styles.head}>Available Emergency Call Service</Text>
+        <ScrollView>
+          <View style={styles.container}>
+            <Pressable
+              style={styles.pressable}
+              android_ripple={{ color: "red" }}
+              onPress={() => {
+                makePhoneCall("tel:102");
+              }}
+            >
+              <Image
+                source={require("../assets/images/ambulance.png")}
+                style={styles.image}
+              />
+              <Text style={styles.text}>Ambulance</Text>
+            </Pressable>
 
-<View style={styles.container}>
-      <Pressable style={styles.pressable} android_ripple={{ color: 'red' }} onPress={()=>{makePhoneCall('tel:102')}}>
-        <Image
-          source={require('../assets/images/ambulance.png')}
-          style={styles.image}
-        />
-        <Text style={styles.text}>Ambulance</Text>
-      </Pressable>
+            <Pressable
+              style={styles.pressable}
+              android_ripple={{ color: "red" }}
+              onPress={() => {
+                makePhoneCall("tel:101");
+              }}
+            >
+              <Image
+                source={require("../assets/images/fire.png")}
+                style={styles.image}
+              />
 
-      <Pressable style={styles.pressable} android_ripple={{ color: 'red' }} onPress={()=>{makePhoneCall('tel:101')}}>
-        
-        <Image
-          source={require('../assets/images/fire.png')}
-          style={styles.image}
-        />
-        
-        <Text style={styles.text}>Fire Brigade</Text>
-      </Pressable>
+              <Text style={styles.text}>Fire Brigade</Text>
+            </Pressable>
 
-      <Pressable style={styles.pressable} android_ripple={{ color: 'red' }} onPress={()=>{makePhoneCall('tel:100')}}>
-       
-        <Image
-          source={require('../assets/images/police.jpg')}
-          style={styles.image}
-        />
-        <Text style={styles.text}>Police Services</Text>
-      </Pressable>
+            <Pressable
+              style={styles.pressable}
+              android_ripple={{ color: "red" }}
+              onPress={() => {
+                makePhoneCall("tel:100");
+              }}
+            >
+              <Image
+                source={require("../assets/images/police.jpg")}
+                style={styles.image}
+              />
+              <Text style={styles.text}>Police Services</Text>
+            </Pressable>
 
-      <Pressable style={styles.pressable} android_ripple={{ color: 'red' }} onPress={()=>{makePhoneCall('tel:9841007074 ')}}>
-        <Image
-          source={require('../assets/images/animals.png')}
-          style={styles.image}
-        />
-        <Text style={styles.text}>Animal Rescue</Text>
-      </Pressable>
-    </View>
-    </ScrollView>
-    </Application>
-</>
+            <Pressable
+              style={styles.pressable}
+              android_ripple={{ color: "red" }}
+              onPress={() => {
+                makePhoneCall("tel:9841007074 ");
+              }}
+            >
+              <Image
+                source={require("../assets/images/animals.png")}
+                style={styles.image}
+              />
+              <Text style={styles.text}>Animal Rescue</Text>
+            </Pressable>
+          </View>
+        </ScrollView>
+      </Application>
+    </>
+  );
+};
 
-    )
-}
-
-export default Call
+export default Call;
--- layout/application.js
+++ layout/application.js
@@ -1,68 +1,104 @@
-import { Text, View, TouchableOpacity,StyleSheet,Dimensions } from "react-native"
-import {def,ser} from './default.js'
-import { useContext } from "react"
+import {
+  Text,
+  View,
+  TouchableOpacity,
+  StyleSheet,
+  Dimensions,
+} from "react-native";
+import { def, ser } from "./default.js";
+import { useContext, useState } from "react";
 
+import context from "../context/maincontext.js";
+export const hei = Dimensions.get("window").height;
+export const wid = Dimensions.get("window").width;
 
-import context from "../context/maincontext.js"
-export const hei=Dimensions.get("window").height;
-export const wid=Dimensions.get("window").width;
+const Application = ({ children }) => {
+  const [pressed, setPressed] = useState(null);
+  const { page, setPage, isServer } = useContext(context);
 
-
-
-
-const Application = ({children}) => {
-  
-    const {page,setPage,isServer}=useContext(context)
   return (
     <View style={styles.main}>
-    
-        <View style={styles.child}>{children}</View>
-        {!isServer?<View style={styles.nav}>
-        {def.map((n)=>{
-            return(<TouchableOpacity  key={n.name} style={styles.ele} onPress={()=>{setPage(n.name)}}><Text style={styles.text}>{n.name}</Text></TouchableOpacity>)
-        })}
-    </View>:<View style={styles.nav}>
-        {ser.map((n)=>{
-            return(<TouchableOpacity  key={n.name} style={styles.ele} onPress={()=>{setPage(n.name)}}><Text style={styles.text}>{n.name}</Text></TouchableOpacity>)
-        })}
-    </View>}
-    
+      <View style={styles.child}>{children}</View>
+
+      {!isServer ? (
+        <View style={styles.nav}>
+          {def.map((n) => {
+            return (
+              <TouchableOpacity
+                key={n.name}
+                style={[
+                  styles.ele,
+                  pressed === n.name && {
+                    backgroundColor: "blue", // Much darker background when pressed
+                  },
+                ]}
+                onPressIn={() => setPressed(n.name)} // Set the pressed button id
+                onPressOut={() => setPressed(null)}
+                onPress={() => {
+                  setPage(n.name);
+                }}
+              >
+                <Text style={styles.text}>{n.name}</Text>
+              </TouchableOpacity>
+            );
+          })}
+        </View>
+      ) : (
+        <View style={styles.nav}>
+          {ser.map((n) => {
+            return (
+              <TouchableOpacity
+                key={n.name}
+                style={[
+                  styles.ele,
+                  pressed === n.name && { backgroundColor: "blue" },
+                ]}
+                onPressIn={() => setPressed(n.name)} // Set the pressed button id
+                onPressOut={() => setPressed(null)}
+                onPress={() => {
+                  setPage(n.name);
+                }}
+              >
+                <Text style={styles.text}>{n.name}</Text>
+              </TouchableOpacity>
+            );
+          })}
+        </View>
+      )}
     </View>
-  )
-}
-const styles=StyleSheet.create({
-
-  main:{
-marginTop:40,
-
+  );
+};
+const styles = StyleSheet.create({
+  main: {
+    // marginTop: 10,
   },
 
-  child:{
-   
-   paddingTop:20,
-   backgroundColor:'#001133',
-height:hei*0.95
+  child: {
+    paddingTop: 20,
+    backgroundColor: "#001133",
+    height: hei * 0.95,
   },
-  nav:{
-    backgroundColor:"black",
-    justifyContent:"space-around",
-    flexDirection:"row",
- 
-    // height:"100%",
-    
-    width:"100%",
-    
+  nav: {
+    backgroundColor: "#3c4f66",
+    flexDirection: "row",
+    width: "100%",
+    height: "5%",
   },
-  ele:{
-    paddingVertical:20
+  ele: {
+    padding: 10,
+    borderStyle: "solid",
+    borderLeftWidth: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    borderRightWidth: 1,
+    flex: 1 / 3,
+    borderColor: "  red",
   },
-  text:{
-    color:"white",
-    fontFamily:"Roboto",
-    fontWeight:"bold"
-    
-  }
-})
+  text: {
+    color: "white",
+    fontFamily: "Roboto",
+    fontWeight: "bold",
+  },
+});
 
-
-export default Application
+export default Application;
